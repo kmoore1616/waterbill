0715a00ac6c183f2265b744aa4653ec5953ab2cb
Kyle <kyle.moore2221@gmail.com>
2025-09-10 15:13:52 -0600

    fixed git

--- src/Customer.java
+++ src/Customer.java
@@ -23,17 +23,19 @@ public class Customer {
         }
     }
 
+    final double SINGLE_BASE = 13.21;
+
     public void calculateBill() {
 
 
         if (customerType == 1) {
             if (gallonsUsed <= 7000) {
-                bill = 13.21 + gallonsUsed * (2.04 / 1000.0);
+                bill = SINGLE_BASE+ gallonsUsed * (2.04 / 1000.0);
             } else if (gallonsUsed <= 13000) {
-                bill = 13.21 + 7000 * (2.04 / 1000.0)
+                bill =  SINGLE_BASE + 7000 * (2.04 / 1000.0)
                         + (gallonsUsed - 7000) * (2.35 / 1000.0);
             } else {
-                bill = 13.21 + 7000 * (2.04 / 1000.0)
+                bill = SINGLE_BASE + 7000 * (2.04 / 1000.0)
                         + 6000 * (2.35 / 1000.0)
                         + (gallonsUsed - 13000) * (2.70 / 1000.0);
             }
